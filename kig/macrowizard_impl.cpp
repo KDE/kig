ae88d5ed060dd0e8a94c063b4514693dfa3a2f56
Dominique Devriese <devriese@kde.org>
2002-04-18 06:12:12 +0000

    Forgot to cvs add some files
    svn path=/trunk/kdeedu/kig/; revision=150201

---
+++ kig/macrowizard_impl.cpp
@@ -0,0 +1,32 @@
+#include "macrowizard_impl.h"
+
+#include "kig_part.h"
+
+/* 
+ *  Constructs a MacroWizardImpl which is a child of 'parent', with the 
+ *  name 'name' and widget flags set to 'f' 
+ *
+ *  The wizard will by default be modeless, unless you set 'modal' to
+ *  TRUE to construct a modal wizard.
+ */
+MacroWizardImpl::MacroWizardImpl( KigDocument* inDoc)
+  : MacroWizard( inDoc->widget(), "Macro Wizard", false, WType_TopLevel | WStyle_DialogBorder | WStyle_Title | WStyle_Dialog ),
+    document(inDoc)
+{
+  setNextEnabled(m_pGivenObjsPage, false);
+  setNextEnabled(m_pFinalObjsPage, false);
+  setFinishEnabled(m_pNameTypePage, false);
+}
+
+/*  
+ *  Destroys the object and frees any allocated resources
+ */
+MacroWizardImpl::~MacroWizardImpl()
+{
+    // no need to delete child widgets, Qt does it all for us
+}
+
+void MacroWizardImpl::setFinishable(const QString& s)
+{
+  if (!s.isEmpty()) setFinishEnabled( m_pNameTypePage, true );
+}
---
+++ kig/macrowizard_impl.h
@@ -0,0 +1,36 @@
+#ifndef MACROWIZARDIMPL_H
+#define MACROWIZARDIMPL_H
+#include "macrowizard.h"
+
+class KigDocument;
+
+class MacroWizardImpl : public MacroWizard
+{ 
+    Q_OBJECT
+
+protected:
+    KigDocument* document;
+public:
+    MacroWizardImpl( KigDocument* parent);
+    ~MacroWizardImpl();
+  
+public slots:
+  void setFinishable(const QString&);
+  
+  // we reimplement the QDialog reject() to emit the canceled() signal
+  virtual void reject() { emit canceled(); MacroWizard::reject(); };
+  // we also reimplement the QWizard::next() and back() functions
+  virtual void next() { emit stepMacro(); MacroWizard::next(); };
+  virtual void back() {};
+  // we also reimplement QDialog::accept() to emit the stepMacro() signal
+  virtual void accept() { emit finishMacro(); MacroWizard::accept(); };
+
+signals:
+  void canceled();
+  void stepMacro();
+  void stepBackMacro();
+  void finishMacro();
+
+};
+
+#endif // MACROWIZARDIMPL_H
