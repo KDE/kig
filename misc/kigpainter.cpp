b124522b220ee99a56fd9ed258ddb594b2d20607
Dominique Devriese <devriese@kde.org>
2002-08-17 17:28:53 +0000

    fixing the inconsistent size of points... ( bypassing the rounding which caused that... ) CVS_SILENT CVS_SILENTing this since most people won't be interested in Kig bugfixes, i suppose :)
    svn path=/trunk/kdeedu/kig/; revision=172402

--- misc/kigpainter.cpp
+++ misc/kigpainter.cpp
@@ -82,7 +82,12 @@ void KigPainter::drawPoint( const Coordinate& p, bool s )
   else
   {
     setBrushStyle( Qt::SolidPattern );
-    drawCircle( p, 2*pixelWidth() );
+// rounding probs: this leads to a non-consistent size of the points...
+//    drawCircle( p, 2*pixelWidth() );
+    QRect r( 0, 0, 5, 5 );
+    r.moveCenter( toScreen( p ) );
+    mP.drawEllipse( r );
+    if( mNeedOverlay ) mOverlay.push_back( r );
   }
 }
 
