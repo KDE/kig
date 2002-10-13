356b937223733a1a0005aabdbf37a7161cbf2902
Dominique Devriese <devriese@kde.org>
2002-10-13 16:03:39 +0000

    damn, apparently i forgot some places in the previous address change, setting them to the devriese@kde.org one too...
    svn path=/trunk/kdeedu/kig/; revision=183704

--- kig/exporttoimagedialog.h
+++ kig/exporttoimagedialog.h
@@ -1,5 +1,5 @@
 // exporttoimagedialog.h
-// Copyright (C)  2002  Dominique Devriese <fritmebufstek@pandora.be>
+// Copyright (C)  2002  Dominique Devriese <devriese@kde.org>
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
--- misc/coordinate.cpp
+++ misc/coordinate.cpp
@@ -1,5 +1,5 @@
 // coordinate.cpp
-// Copyright (C)  2002  Dominique Devriese <fritmebufstek@pandora.be>
+// Copyright (C)  2002  Dominique Devriese <devriese@kde.org>
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
