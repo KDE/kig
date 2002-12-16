8339b32c0729bc0030917122a34713f53685a8f0
Dominique Devriese <devriese@kde.org>
2002-12-16 23:31:25 +0000

    add two new classes: ArgsParser: a generic arguments parser object and NewType: I'm splitting up the Type class into two classes: NewType ( will rename this, prolly ), which represents a C++ type, that the user doesn't know about, but that we use to save and load, etc. And Constructible: this represents what appears to the user as a type.  Not sure what that one will look like yet..
    svn path=/trunk/kdeedu/kig/; revision=194373

---
+++ misc/argsparser.cpp
@@ -0,0 +1,71 @@
+// argsparser.cpp
+// Copyright (C)  2002  Dominique Devriese <devriese@kde.org>
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
+// 02111-1307, USA.
+
+#include "argsparser.h"
+
+#include "../objects/object.h"
+
+#include <algorithm>
+
+ArgParser::ArgParser( const spec args[] )
+{
+  for ( const spec* s = args; s != 0; ++s )
+    mmap[s->type] = s->number;
+  mwantedobjscount = 0;
+  for ( const spec* s = args; s != 0; ++s )
+    mwantedobjscount += s->number;
+}
+
+int ArgParser::check( const Objects& os )
+{
+  // we take a copy, so we can change its contents..
+  maptype map = mmap;
+  for ( Objects::const_iterator i = os.begin(); i != os.end(); ++i )
+  {
+    maptype::iterator r = map.find( (*i)->type() );
+    if ( r == map.end() )       // an obj of a type we don't want
+      return 0;
+    else if ( r->second <= 0 )  // too much objs of a type that we do
+                                // want..
+      return 0;
+    else r->second--;           // a good obj
+  };
+  for ( maptype::const_iterator i = map.begin(); i != map.end(); ++i )
+    if ( i->second > 0 ) return Valid; // we need more objs
+  return Valid | Complete;      // and we're done..
+}
+
+Objects ArgParser::parse( const Objects& os )
+{
+  assert( check( os ) & Valid );
+
+  Objects ret( mwantedobjscount, static_cast<Object*>( 0 ) );
+
+  int count = 0;
+  maptype map = mmap;
+  for ( maptype::iterator i = map.begin(); i != map.end(); ++i )
+  {
+    count += i->second;
+    i->second = count;
+  };
+
+  for ( Objects::const_iterator i = os.begin(); i != os.end(); ++i )
+    ret[--map[(*i)->type()]] = *i;
+
+  return ret;
+}
