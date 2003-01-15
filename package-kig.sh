be9dce474ffcc978eacc08ff4717e025f14e7dfa
Dominique Devriese <devriese@kde.org>
2003-01-15 13:46:14 +0000

    a big part of package-kig.sh is obsolete after my mods to cvs2dist..
    svn path=/trunk/kdeedu/kig/; revision=200086

--- package-kig.sh
+++ package-kig.sh
@@ -1,36 +1,24 @@
 #! /bin/bash
 
 # this is mostly a log for myself for remembering how to build the kig
-# packages.  There are a lot of options to Jason Katz-Brown and
-# Sebastain Stein's cvs2dist program (
-# http://www.katzbrown.com/shiritsu/programming/cvs2dist/ ) and i
-# wrote some scripts to remember what options i want..
+# packages.
 
 OLDPWD=$(pwd)
 
 VERSION="0.3"
 NAME="kig"
-I18NDIR="/home/domi/src/kde-i18n"
-ADMINDIR="/home/domi/src/kde-common/admin"
 
-TEMPDIR="/tmp/$NAME-$VERSION-package-temp"
+TEMPDIR="/tmp/$NAME-package-temp"
 rm -rf $TEMPDIR
 mkdir $TEMPDIR
 cd $TEMPDIR
 
-cd $I18NDIR
-I18NLANGS=$(find -name 'kig.po' | grep -v docs | sed -e 's#\./\([^/]*\)/.*#\1#')
-echo $I18NLANGS
-
 cd $TEMPDIR
-~domi/downloads/cvs2dist \
+~domi/src/cvs2dist \
 	--name "$NAME" \
 	--version "$VERSION" \
-	--admin-dir="$ADMINDIR" \
-	--i18n-dir "$I18NDIR" \
-	--i18n-langs="$I18NLANGS" \
 	--log="$TEMPDIR/log" \
-	~/src/test/kdenonbeta kig
+	~/src/kdeedu kig
 
 cd $OLDPWD
 
