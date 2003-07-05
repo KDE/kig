b518459f05de85047f4f0275f5792d04b190965f
Dirk Mueller <mueller@kde.org>
2003-07-05 21:27:22 +0000

    update FSF address
    svn path=/trunk/kdeedu/kig/; revision=235629

--- filters/kseg-defs.h
+++ filters/kseg-defs.h
@@ -13,8 +13,8 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA
-// 02111-1307, USA.
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston,  
+// MA  02111-1307, USA.
 
 // this is a collection of definitions we need from KSeg.  It includes
 // code from defs.H and G_drawstyle.H.  Thanks to Ilya Baran for
