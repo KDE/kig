973054ad8ec50115d90601e1d0020b2b2363004c
Dominique Devriese <devriese@kde.org>
2003-08-03 15:17:27 +0000

    fix the Coordinate documentation for doxygen
    svn path=/trunk/kdeedu/kig/; revision=240738

--- misc/coordinate.h
+++ misc/coordinate.h
@@ -35,64 +35,58 @@ class Coordinate
 public:
   static Coordinate fromQPoint( const QPoint& p );
 
-  /** Create an invalid Coordinate
-   * Returns an invalid Coordinate.  This is a special value of a
-   * Coordinate that signals that something went wrong..
-   * \see Coordinate::valid
-   * \internal
-   * we represent an invalid coordinate by setting x or y to positive
-   * or negative infinity.  This is handy, since it doesn't require us
-   * to adapt most of the functions, it doesn't need extra space, and
-   * most of the times that we should get an invalid coord, we get one
-   * automatically..
-   */
-  static Coordinate invalidCoord();
-  /** Is this a valid Coordinate
-   * Returns whether this Coordinate is valid.
-   * \see Coordinate::invalidCoord
-   */
-  bool valid() const;
-
-  /** Constructor
-   * Construct a new Coordinate, with a given x and y value
+  /** Constructor.  Construct a new Coordinate, with a given x and y
+   * value.
    */
   Coordinate( const double x, const double y );
-  /** Copy Constructor
-   *
-   * Construct a new Coordinate, and give it the same value as p.
+  /** Copy Constructor.  Construct a new Coordinate, and give it the
+   *  same value as p.
    */
   Coordinate( const Coordinate& p );
   /**
-   * \if creating-python-scripting-doc
+   * \ifnot creating-python-scripting-doc
    * \brief Default Constructor
+   *
    * Constructs a new Coordinate, with x and y initialized to 0.
    * \endif
    */
   Coordinate();
   ~Coordinate() {};
 
-  /** Distance to another Coordinate
-   * Returns the distance to another Coordinate.
+  /** Create an invalid Coordinate.  This is a special value of a
+   * Coordinate that signals that something went wrong..
+   *
+   * \see Coordinate::valid
+   *
+   * \internal We represent an invalid coordinate by setting x or y to
+   * positive or negative infinity.  This is handy, since it doesn't
+   * require us to adapt most of the functions, it doesn't need extra
+   * space, and most of the times that we should get an invalid coord,
+   * we get one automatically..
+   */
+  static Coordinate invalidCoord();
+  /** Is this a valid Coordinate.
+   * \see Coordinate::invalidCoord
+   */
+  bool valid() const;
+
+  /** Distance to another Coordinate.
    */
   double distance ( const Coordinate& p ) const;
-  /** Length
-   * Returns the length or norm of this coordinate.  I.e. return the
-   * distance from this Coordinate to the origin.
+  /** Length.  Returns the length or norm of this coordinate.
+   * I.e. return the distance from this Coordinate to the origin.
    * \see squareLength
    */
   double length () const;
-  /** Square length
-   * Equivalent to the square of \ref length, but a bit more efficient
-   * because no square root has to be calculated.
+  /** Square length.  Equivalent to the square of \ref length, but a
+   * bit more efficient because no square root has to be calculated.
    * \see length
    */
   inline double squareLength() const;
-  /** Inverse
-   * Returns the inverse of this Coordinate
+  /** Inverse.  Returns the inverse of this Coordinate.
    */
   const Coordinate operator- () const;
-  /** Orthogonal
-   * Returns a vector which is orthogonal on this vector
+  /** Orthogonal.  Returns a vector which is orthogonal on this vector.
    * This relation always holds:
    * <pre>
    * Coordinate a = ...;
@@ -100,75 +94,60 @@ public:
    * </pre>
    */
   const Coordinate orthogonal() const;
-  /** Round
-   * Returns this coordinate, rounded down to the nearest integral
+  /** Round. Returns this coordinate, rounded to the nearest integral
    * values.
    */
   const Coordinate round() const;
-  /** Normalize
-   * this sets our length to length, while keeping the x/y ratio
-   * untouched...
+  /** Normalize.  This sets the length to length, while keeping the
+   * x/y ratio untouched...
    */
   const Coordinate normalize( double length = 1 ) const;
   QPoint toQPoint() const;
 
   Coordinate& operator= ( const Coordinate& c );
-  /** Add
-   * Add c to this Coordinate
+  /** Add.  Add c to this Coordinate
    */
   Coordinate& operator+= ( const Coordinate& c );
-  /** Subtract
-   * Subtract c from this Coordinate
+  /** Subtract.  Subtract c from this Coordinate
    */
   Coordinate& operator-= ( const Coordinate& c );
-  /** Scale
-   * Scales this Coordinate by a factor r
+  /** Scale.  Scales this Coordinate by a factor r
    */
   Coordinate& operator*= ( const double r );
-  /** Scale
-   * Scales this Coordinate by a factor r
+  /** Scale.  Scales this Coordinate by a factor r
    */
   Coordinate& operator*= ( const int r );
-  /** Scale
-   * Scales this Coordinate by a factor 1/r
+  /** Scale.  Scales this Coordinate by a factor 1/r
    */
   Coordinate& operator/= ( const double r );
 public:
-  /** X Component
-   * The X Component of this Coordinate
+  /** X Component.  The X Component of this Coordinate.
    */
   double x;
-  /** Y Component
-   * The Y Component of this Coordinate
+  /** Y Component.  The Y Component of this Coordinate.
    */
   double y;
+
   friend kdbgstream& operator<<( kdbgstream& s, const Coordinate& t );
-  /** Add
-   * Returns the sum of a and b
+  /** Add.  Returns the sum of a and b.
    */
   friend const Coordinate operator+ ( const Coordinate& a, const Coordinate& b );
-  /** Subtract
-   * Returns the difference between a and b
+  /** Subtract.  Returns the difference between a and b.
    */
   friend const Coordinate operator- ( const Coordinate& a, const Coordinate& b );
-  /** Scale
-   * Returns this a, scaled by a factor of r
+  /** Scale.  Returns this a, scaled by a factor of r.
    */
   friend const Coordinate operator* ( const Coordinate& a, const double r );
-  /** Scale
-   * Returns a, scaled by a factor of 1/r
+  /** Scale.  Returns a, scaled by a factor of 1/r.
    */
   friend const Coordinate operator/ ( const Coordinate& a, const double r );
-  /** Scalar Product
-   * Returns the scalar product of a and b
+  /** Scalar Product.  Returns the scalar product of a and b.
    */
   friend double operator*( const Coordinate& a, const Coordinate& b );
-  /** Equal
-   * Tests two Coordinates for equality
+  /** Equal.  Tests two Coordinates for equality.
    */
   friend bool operator==( const Coordinate&, const Coordinate& );
-  /** Not Equal
-   * Tests two Coordinates for inequality
+  /** Not Equal.  Tests two Coordinates for inequality.
    */
   friend bool operator!=( const Coordinate&, const Coordinate& );
 };
