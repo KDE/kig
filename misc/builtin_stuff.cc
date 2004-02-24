df486e1860a3dbc718f1ffe33c0f1f8daffd6b34
Maurizio Paolini <paolini@dmf.unicatt.it>
2004-02-24 21:09:36 +0000

    added four icons for the four test actions
    svn path=/trunk/kdeedu/kig/; revision=291056

--- misc/builtin_stuff.cc
+++ misc/builtin_stuff.cc
@@ -385,22 +385,26 @@ void setupBuiltinStuff()
 
     // tests
     c = new TestConstructor( AreParallelType::instance(), "Parallel Test",
-                             "Test whether two given lines are parallel", "" );
+                             "Test whether two given lines are parallel", 
+			     "testparallel" );
     ctors->add( c );
     actions->add( new ConstructibleAction( c, "objects_new_areparallel" ) );
 
     c = new TestConstructor( AreOrthogonalType::instance(), "Orthogonal Test",
-                             "Test whether two given lines are orthogonal", "" );
+                             "Test whether two given lines are orthogonal", 
+		             "testorthogonal" );
     ctors->add( c );
     actions->add( new ConstructibleAction( c, "objects_new_areorthogonal" ) );
 
     c = new TestConstructor( AreCollinearType::instance(), "Collinear Test",
-                             "Test whether three given points are collinear", "" );
+                             "Test whether three given points are collinear", 
+			     "testcollinear" );
     ctors->add( c );
     actions->add( new ConstructibleAction( c, "objects_new_arecollinear" ) );
 
     c = new TestConstructor( ContainsTestType::instance(), "Contains Test",
-                             "Test whether a given curve contains a given point", "" );
+                             "Test whether a given curve contains a given point", 
+                             "testcontains" );
 
     ctors->add( c );
     actions->add( new ConstructibleAction( c, "objects_new_containstest" ) );
