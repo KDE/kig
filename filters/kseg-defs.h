0683d5e3de8b41a431e5144e6ec99a9be484f2d0
Dirk Mueller <mueller@kde.org>
2005-08-23 14:29:44 +0000

    update FSF address
    svn path=/branches/KDE/3.5/kdeedu/kig/; revision=452492

--- filters/kseg-defs.h
+++ filters/kseg-defs.h
@@ -35,7 +35,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Foundation, Inc., 51 Franklin Steet, Fifth Floor, Cambridge, MA 02110-1301, USA.
  *
  *  Send comments and/or bug reports to:
  *                 ibaran@mit.edu
@@ -280,7 +280,7 @@ enum MenuIDs
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+ *  Foundation, Inc., 51 Franklin Steet, Fifth Floor, Cambridge, MA 02110-1301, USA.
  *
  *  Send comments and/or bug reports to:
  *                 ibaran@mit.edu
