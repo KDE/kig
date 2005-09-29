370ce50a8fe7e7adc11d74cd251d60661c811071
Dirk Mueller <mueller@kde.org>
2005-09-29 14:33:08 +0000

    SVN_SILENT: fix typo
    svn path=/branches/KDE/3.5/kdeedu/kig/; revision=465369

--- filters/kseg-defs.h
+++ filters/kseg-defs.h
@@ -12,7 +12,7 @@
 
 // You should have received a copy of the GNU General Public License
 // along with this program; if not, write to the Free Software
-// Foundation, Inc., 51 Franklin Steet, Fifth Floor, Boston,  
+// Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,  
 // MA  02110-1301, USA.
 
 // this is a collection of definitions we need from KSeg.  It includes
@@ -35,7 +35,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Steet, Fifth Floor, Cambridge, MA 02110-1301, USA.
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.
  *
  *  Send comments and/or bug reports to:
  *                 ibaran@mit.edu
@@ -280,7 +280,7 @@ enum MenuIDs
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 51 Franklin Steet, Fifth Floor, Cambridge, MA 02110-1301, USA.
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Cambridge, MA 02110-1301, USA.
  *
  *  Send comments and/or bug reports to:
  *                 ibaran@mit.edu
