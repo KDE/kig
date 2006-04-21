970979f2b8cf484a371bcbec07c09586aa0828e4
Pino Toscano <pino@kde.org>
2006-04-21 17:43:47 +0000

    Almost complete attempt to make kig compilable again with boost+python.
    svn path=/trunk/KDE/kdeedu/kig/; revision=532334

--- scripting/script-common.cc
+++ scripting/script-common.cc
@@ -47,7 +47,7 @@ QString ScriptType::templateCode( ScriptType::Type type, std::set<ObjectHolder*>
   {
     QString tempcode = QString::fromLatin1( "def calc( " );
     bool firstarg = true;
-    QString temparg = ki18nc( "Note to translators: this should be a default "
+    KLocalizedString temparg = ki18nc( "Note to translators: this should be a default "
                             "name for an argument in a Python function. The "
                             "default is \"arg%1\" which would become arg1, "
                             "arg2, etc. Give something which seems "
