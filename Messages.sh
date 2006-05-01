e74e8d5903e44a51e58983ea1ec564382472ad63
Stephan Kulow <coolo@kde.org>
2006-05-01 19:20:58 +0000

    another moved messages target
    svn path=/trunk/KDE/kdeedu/kig/; revision=536293

---
+++ Messages.sh
@@ -0,0 +1,15 @@
+#! /bin/sh
+rm -f tips.cpp
+$EXTRACTRC */*.rc >> rc.cpp || exit 11
+$EXTRACTRC */*.ui >> rc.cpp || exit 12
+(cd data && $PREPARETIPS > ../tips.cpp)
+for file in macros/*.kigt; do
+	cat "$file" | grep '<Name>' | sed -e 's/^ *<Name>\([^<]*\)<\/Name>/i18n( "\1" );/' | sed -e 's/&amp;/\&/g' >> rc.cpp
+	cat "$file" | grep '<Description>' | sed -e 's/^ *<Description>\([^<]*\)<\/Description>/i18n( "\1" );/' | sed -e 's/&amp;/\&/g' >> rc.cpp
+	cat "$file" | grep '<UseText>' | sed -e 's/^ *<UseText>\([^<]*\)<\/UseText>/i18n( "\1" );/' | sed -e 's/&amp;/\&/g' >> rc.cpp
+	cat "$file" | grep '<SelectStatement>' | sed -e 's/^ *<SelectStatement>\([^<]*\)<\/SelectStatement>/i18n( "\1" );/' | sed -e 's/&amp;/\&/g' >> rc.cpp
+done
+$XGETTEXT tips.cpp rc.cpp filters/*.h kig/*.h misc/*.h modes/*.h objects/*.h scripting/*.h */*.cc kig/*.cpp misc/*.cpp modes/*.cpp -o $podir/kig.pot
+$XGETTEXT kfile/kfile_drgeo.cpp kfile/kfile_drgeo.h rc.cpp -o $podir/kfile_drgeo.pot
+$XGETTEXT kfile/kfile_kig.cpp kfile/kfile_kig.h rc.cpp -o $podir/kfile_kig.pot
+
