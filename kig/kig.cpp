7047c5325b1222287e94db9918d6bd285d77b6aa
Stephan Kulow <coolo@kde.org>
2006-08-28 19:59:39 +0000

    replaced several URLs with Url and URI with Uri - the patch became a bit larger than expected, so I couldn't have the time to check if it still compiles with the changes done in between
    svn path=/trunk/KDE/kdeedu/kig/; revision=578269

--- kig/kig.cpp
+++ kig/kig.cpp
@@ -146,7 +146,7 @@ void Kig::load( const KUrl& url )
 {
   // we check for m_part not being 0, because in the case of us not
   // finding our library, we would otherwise get a crash...
-  if ( m_part && m_part->openURL( url ) ) m_recentFilesAction->addUrl( url );
+  if ( m_part && m_part->openUrl( url ) ) m_recentFilesAction->addUrl( url );
 }
 
 void Kig::fileNew()
