20abcd79289c260922ae2a9810af304562846972
Pino Toscano <pino@kde.org>
2007-08-19 14:47:05 +0000

    If the loading of the document fails, completely cleanup ourselves to be able to open a new document in the same (untouched) window.
    svn path=/branches/KDE/3.5/kdeedu/kig/; revision=701849

--- kig/kig_part.cpp
+++ kig/kig_part.cpp
@@ -399,7 +399,12 @@ bool KigPart::openFile()
   };
 
   KigDocument* newdoc = filter->load (m_file);
-  if ( !newdoc ) return false;
+  if ( !newdoc )
+  {
+    closeURL();
+    m_url = KURL();
+    return false;
+  }
   delete mdocument;
   mdocument = newdoc;
   coordSystemChanged( mdocument->coordinateSystem().id() );
