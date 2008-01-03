949e6fb77b1313ac68bc714dc6e01bc23be011b4
Pino Toscano <pino@kde.org>
2008-01-03 22:30:05 +0000

    try to be positive and bump the version to 1.0
    svn path=/trunk/KDE/kdeedu/kig/; revision=756925

--- config-kig.h.cmake
+++ config-kig.h.cmake
@@ -2,7 +2,7 @@
 #cmakedefine HAVE_TRUNC 1
 
 /* The current Kig version as a string */
-#define KIGVERSION "0.90.0"
+#define KIGVERSION "1.0"
 
 /* Defined if Kig Python scripting is enabled */
 #cmakedefine KIG_ENABLE_PYTHON_SCRIPTING 1
