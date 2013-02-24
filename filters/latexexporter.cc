752086ce40a5b19fa594381aed87b70323023005
David E. Narváez <david.narvaez@computer.org>
2013-02-24 00:11:20 -0500

    Removing strange assign sequence, probably typos

--- filters/latexexporter.cc
+++ filters/latexexporter.cc
@@ -788,7 +788,7 @@ void LatexExporter::run( const KigPart& doc, KigWidget& w )
         {
             // TODO: Polar grid
             // vertical lines...
-            double startingpoint = startingpoint = static_cast<double>( KDE_TRUNC( left ) );
+            double startingpoint = static_cast<double>( KDE_TRUNC( left ) );
             for ( double i = startingpoint; i < left+width; ++i )
             {
                 stream << "draw((" << i << "," << bottom << ")--(" << i << "," << bottom+height << "),gray);\n";
