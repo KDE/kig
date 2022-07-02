15879016d938b2e574e2cf5aca19d3595f08b6c6
Laurent Montel <montel@kde.org>
2022-07-02 13:35:54 +0200

    Fix adapt for loading plugin in kf6

--- kig/main.cpp
+++ kig/main.cpp
@@ -25,7 +25,7 @@
 
 static int convertToNative( const QUrl &file, const QByteArray& outfile )
 {
-  KPluginLoader libraryLoader( QStringLiteral("kf5/parts/kigpart") );
+  KPluginLoader libraryLoader( QStringLiteral("kf" QT_STRINGIFY(QT_VERSION_MAJOR)) + QStringLiteral("/parts/kigpart") );
   QLibrary library( libraryLoader.fileName() );
   int ( *converterfunction )( const QUrl&, const QByteArray& );
   converterfunction = ( int ( * )( const QUrl&, const QByteArray& ) ) library.resolve( "convertToNative" );
